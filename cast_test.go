6796452c69f28004236327cb5bd80728f5be356d
spf13 <steve.francia@gmail.com>
2014-04-05 01:21:52 -0400

    initial commit of library & tests

---
+++ cast_test.go
@@ -0,0 +1,26 @@
+// Copyright © 2014 Steve Francia <spf@spf13.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package cast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToInt(t *testing.T) {
+	var eight interface{} = 8
+	assert.Equal(t, ToInt(8), 8)
+	assert.Equal(t, ToInt("8"), 8)
+	assert.Equal(t, ToInt(true), 1)
+	assert.Equal(t, ToInt(false), 0)
+	assert.Equal(t, ToInt(eight), 8)
+}
+
+func TestMaps(t *testing.T) {
+	var taxonomies = map[interface{}]interface{}{"tag": "tags", "group": "groups"}
+	assert.Equal(t, ToStringMap(taxonomies), map[string]interface{}{"tag": "tags", "group": "groups"})
+}
