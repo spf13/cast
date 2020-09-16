6cd11a28a2684d817e45f0aea4240cff414233bb
Valeriy Solovyov <weldpua2008@gmail.com>
2020-09-16 23:33:31 +0300

    Fixing style

--- caste.go
+++ caste.go
@@ -1273,9 +1273,9 @@ func parseDateWith(s string, dates []string) (d time.Time, e error) {
 			return
 		}
 	}
-    if i, err := strconv.Atoi(s); err == nil {
-        return time.Unix(int64(i), 0), nil
-    }
+	if i, err := strconv.Atoi(s); err == nil {
+		return time.Unix(int64(i), 0), nil
+	}
 
 	return d, fmt.Errorf("unable to parse date: %s", s)
 }
