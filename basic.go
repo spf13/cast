35730ea4d88211fb0127fe5e9e424ba148a6120e
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-05-31 19:30:27 +0200

    chore: improve basic types
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- basic.go
+++ basic.go
@@ -14,8 +14,8 @@ import (
 	"time"
 )
 
-// ToBoolE casts an interface to a bool type.
-func ToBoolE(i interface{}) (bool, error) {
+// ToBoolE casts any value to a bool type.
+func ToBoolE(i any) (bool, error) {
 	i = indirect(i)
 
 	switch b := i.(type) {
@@ -25,45 +25,46 @@ func ToBoolE(i interface{}) (bool, error) {
 		return false, nil
 	case int:
 		return b != 0, nil
-	case int64:
-		return b != 0, nil
-	case int32:
+	case int8:
 		return b != 0, nil
 	case int16:
 		return b != 0, nil
-	case int8:
+	case int32:
+		return b != 0, nil
+	case int64:
 		return b != 0, nil
 	case uint:
 		return b != 0, nil
-	case uint64:
-		return b != 0, nil
-	case uint32:
+	case uint8:
 		return b != 0, nil
 	case uint16:
 		return b != 0, nil
-	case uint8:
+	case uint32:
 		return b != 0, nil
-	case float64:
+	case uint64:
 		return b != 0, nil
 	case float32:
+		return b != 0, nil
+	case float64:
 		return b != 0, nil
 	case time.Duration:
 		return b != 0, nil
 	case string:
-		return strconv.ParseBool(i.(string))
+		return strconv.ParseBool(b)
 	case json.Number:
 		v, err := ToInt64E(b)
 		if err == nil {
 			return v != 0, nil
 		}
+
 		return false, fmt.Errorf("unable to cast %#v of type %T to bool", i, i)
 	default:
 		return false, fmt.Errorf("unable to cast %#v of type %T to bool", i, i)
 	}
 }
 
-// ToStringE casts an interface to a string type.
-func ToStringE(i interface{}) (string, error) {
+// ToStringE casts any value to a string type.
+func ToStringE(i any) (string, error) {
 	switch s := i.(type) {
 	case string:
 		return s, nil
@@ -75,24 +76,24 @@ func ToStringE(i interface{}) (string, error) {
 		return strconv.FormatFloat(float64(s), 'f', -1, 32), nil
 	case int:
 		return strconv.Itoa(s), nil
-	case int64:
-		return strconv.FormatInt(s, 10), nil
-	case int32:
-		return strconv.Itoa(int(s)), nil
-	case int16:
-		return strconv.FormatInt(int64(s), 10), nil
 	case int8:
 		return strconv.FormatInt(int64(s), 10), nil
+	case int16:
+		return strconv.FormatInt(int64(s), 10), nil
+	case int32:
+		return strconv.FormatInt(int64(s), 10), nil
+	case int64:
+		return strconv.FormatInt(s, 10), nil
 	case uint:
-		return strconv.FormatUint(uint64(s), 10), nil
-	case uint64:
-		return strconv.FormatUint(uint64(s), 10), nil
-	case uint32:
-		return strconv.FormatUint(uint64(s), 10), nil
-	case uint16:
 		return strconv.FormatUint(uint64(s), 10), nil
 	case uint8:
 		return strconv.FormatUint(uint64(s), 10), nil
+	case uint16:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint32:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint64:
+		return strconv.FormatUint(s, 10), nil
 	case json.Number:
 		return s.String(), nil
 	case []byte:
